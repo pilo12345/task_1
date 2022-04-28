ef337f1b86e105fb84234700f71c0b105dc6561b
Prince Patel <princeboradooo@gmail.com>
2022-04-28 12:49:52 +0530

    did changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:task_1/view/splash_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:task_1/view/switchgetx.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  Directory document = await getApplicationDocumentsDirectory();
+  Hive.init(document.path);
+  await Hive.openBox<String>("todo");
+
   runApp(const MyApp());
 }
 
@@ -18,7 +27,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Task',
-      home: SplashScreen(),
+      home: RRR(),
     );
   }
 }
